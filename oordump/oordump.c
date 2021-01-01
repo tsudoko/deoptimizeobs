3aac019c584195c952149532df8ecde7a29c8bfb
flan <flan@flande.re>
2021-01-01 13:49:06 +0100

    Rename oorextract to oordump
    Extraction doesn't seem like something you usually perform on a
    running process; dumping on the other hand has some established
    usage, e.g. memory/core dumps.

--- oordump/oordump.c
+++ oordump/oordump.c
@@ -81,7 +81,7 @@ find_coptimizedobs_vtable(HMODULE riooor_base)
 __declspec(dllexport) const char *
 GetPluginString(void)
 {
-	return "oorextract.rpo Version 0.00.02\n\tstuff\n\tPublic domain";
+	return "oordump.rpo Version 0.00.02\n\tstuff\n\tPublic domain";
 }
 
 __declspec(dllexport) void *
