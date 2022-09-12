tsudoko/deoptimizeobs

commit 4da3dd1a23059c43c0b531aef9746621408e1f40

Remove redundant byte-order-aware reads
We do a lot of byte-order-unaware reads in other places anyway

oordump/vtable_mfc.c
@@ -41,7 +41,7 @@ char *
 mfc_vtable_classname(void *v)
 {
 	unsigned char *g = ((unsigned char **)v)[0];
-	struct CRuntimeClass *rtc = (void *)(g[1] | (g[2] << 8) | (g[3] << 16) | (g[4] << 24));
+	struct CRuntimeClass *rtc = *((struct CRuntimeClass **)g+1);
 	return rtc->name;
 }
 
@@ -68,7 +68,7 @@ find_mfc_vtable(struct memranges r, char *classname, size_t nclassname, size_t *
 			continue;
 		if(g[0] != MOV_R32_IMM32 || g[5] != RET)
 			continue;
-		struct CRuntimeClass *rtc = (void *)(g[1] | (g[2] << 8) | (g[3] << 16) | (g[4] << 24));
+		struct CRuntimeClass *rtc = *((struct CRuntimeClass **)g+1);
 		if(!is_rtc_candidate(r, rtc))
 			continue;
 

commit 58f7d1d24f66f6148cdb89ba79c5f68ab5ebbd41

Fix broken MFC runtime class detection
Regression introduced in 4da3dd1a23059c43c0b531aef9746621408e1f40.
This line is quite unreadable as-is; no wonder we broke something.

oordump/vtable_mfc.c
@@ -68,7 +68,7 @@ find_mfc_vtable(struct memranges r, char *classname, size_t nclassname, size_t *
 			continue;
 		if(g[0] != MOV_R32_IMM32 || g[5] != RET)
 			continue;
-		struct CRuntimeClass *rtc = *((struct CRuntimeClass **)g+1);
+		struct CRuntimeClass *rtc = *(struct CRuntimeClass **)(g+1);
 		if(!is_rtc_candidate(r, rtc))
 			continue;
 